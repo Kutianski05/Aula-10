d1024ed076ba5cf8b6c0ffa6f4ae3363562e09ba
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-04-26 23:32:35 -0300

    .

--- Atv1/Main.java
+++ Atv1/Main.java
@@ -1,11 +1,15 @@
 package Atv1;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Animal cachorro = new Cachorro("Rex");
-        Animal gato = new Gato("Mingal");
+        ArrayList<Animal> animais = new ArrayList<>();
+        animais.add(new Cachorro("Rex"));
+        animais.add(new Gato("Mingau"));
 
-        cachorro.emitirSom();
-        gato.emitirSom();
+        for (Animal animal : animais) {
+            animal.emitirSom();
+        }
     }
 }
